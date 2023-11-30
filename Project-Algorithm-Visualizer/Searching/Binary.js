32889d5a589b6df000f14ee1801399aa50f99957
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-11-30 22:43:42 +0530

    changes in codes

--- Project-Algorithm-Visualizer/Searching/Binary.js
+++ Project-Algorithm-Visualizer/Searching/Binary.js
@@ -65,50 +65,49 @@ async function descriptionText() {
 
     const code = document.querySelector('#code_java')
     // console.log(code.innerHTML)
-    code.innerHTML = `// Java implementation of recursive Binary Search
-class BinarySearch {
-    // Returns index of x if it is present in arr[l..
-    // r], else return -1
-    int binarySearch(int arr[], int l, int r, int x)
+    code.innerHTML = `// c++ code
+
+    #include<bits/stdc++.h>
+    using namespace std;
+        
+    int BinarySearch(int arr[],int size,int key)
     {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-
-            // If the element is present at the
-            // middle itself
-            if (arr[mid] == x)
-                return mid;
-
-            // If element is smaller than mid, then
-            // it can only be present in left subarray
-            if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-
-            // Else the element can only be present
-            // in right subarray
-            return binarySearch(arr, mid + 1, r, x);
+        int left=0;
+        int right=size-1;
+        int mid=left+(right-left)/2;
+    
+        while(left<=right){
+    
+            if(key==arr[mid]){
+                 return mid;
+            }
+    
+            else if(key>arr[mid])
+            {
+                left=mid+1;
+            }
+    
+            else{
+                right=mid-1;
+            }
+             mid=left+(right-left)/2;
         }
-
-        // We reach here when element is not present
-        // in array
         return -1;
     }
-
-    // Driver method to test above
-    public static void main(String args[])
+    
+    int main()
     {
-        BinarySearch ob = new BinarySearch();
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int n = arr.length;
-        int x = 10;
-        int result = ob.binarySearch(arr, 0, n - 1, x);
-        if (result == -1)
-            System.out.println("Element not present");
-        else
-            System.out.println("Element found at index "
-                            + result);
+    int arr[]={10,20,30,40,50,60};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    
+    int key=20;
+    
+    int k=BinarySearch(arr,size,key);
+    
+    cout<<k<<endl;
+        
+    return 0;
     }
-}
 `
     const time = document.querySelector('#time')
     time.innerHTML = `
